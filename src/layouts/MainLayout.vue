ac6a2c1a44280c16cc33fdb352bcbd0f09850cd1
Even Magnem <evmagn@outlook.com>
2021-04-28 15:15:49 +0200

    initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-layout dark view="hhh lpR fFf">
+    <q-header dark elevated>
+      <q-tabs
+        v-model="tab"
+        inline-label
+      >
+        <!-- <q-route-tab v-for="nav in navs" :key="nav.label" :to="nav.path" :icon="nav.icon" :label="nav.label" /> -->
+      </q-tabs>
+    </q-header>
+    <q-page-container class="bg-grey-5">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      tab: 'home',
+      navs: [
+        { label: 'Home', name: 'home', path: '/', icon: 'home' },
+        { label: 'About', name: 'about', path: '/about', icon: 'info' },
+      ],
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      ref="logo"
+      alt="Quasar logo"
+      src="~assets/logo_teal.svg"
+    >
+  </q-page>
+</template>
+
+<script>
+import gsap from 'gsap';
+export default {
+  mounted() {
+    gsap.from(this.$refs.logo, { opacity: 0, x: -40, scale: 0.7, duration: 3, ease: 'power3.out' });
+  }
+}
+</script>
+
+<style scoped>
+.logo {
+  opacity: 1;
+  transform: scale(1), translateX(0);
+}
+</style>
